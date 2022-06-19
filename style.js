c432e491deb85317686bcbcdf20ecb5dec1a028b
lfonk <jsharp4212@gmail.com>
2022-06-20 01:18:43 +0800

    Add main game loop with score system

--- style.js
+++ style.js
@@ -16,39 +16,71 @@ function computerPlay() {
 // function that will play RPS (only 1 round)
 function playRound(playerSelection, computerSelection){
 // set player and computer parameters 
-    //let playerSelection = prompt("Rock, paper, or scissors? ","").toUpperCase();
-   // let computerSelection = computerPlay();
 // compare player pick to computer pick 
     playerSelection.toLowerCase();
     if (playerSelection == "rock") { 
         if (computerSelection == "Paper"){
-            return "Computer Wins, Paper beats Rock";
+            return "computer";
         } else if (computerSelection == "Scissors"){
-            return "Player Wins, Rock beats Scissors";
+            return "player";
         } else { 
-            return "It's a tie, both picked Rock";
+            return "tie";
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "Paper"){
-            return "It's a tie, both picked Paper";
+            return "tie";
         } else if (computerSelection == "Scissors"){
-            return "Computer Wins, Scissors beats Paper";
+            return "computer";
         } else { 
-            return "Player Wins, Paper beats Rock";
+            return "player";
         }
     } else if (playerSelection == "scissors"){ 
         if (computerSelection == "Paper"){
-            return "Player Wins, Scissors beats Paper";
+            return "player";
         } else if (computerSelection == "Scissors"){
-            return "It's a tie, both picked Scissors";
+            return "tie";
         } else { 
-            return "Computer Wins, Rock beats Scissors";
+            return "computer";
         }
-    } else { // player didnt pick from the
-        return "Try again";
+    } else { // player didnt pick from RPS
+        return "error";
     }
 }
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+//main game loop
+function game() {
+    let computerPoint = 0;
+    let playerPoint = 0; 
+    // loops 5 times
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, or Scissors? ", "").toLowerCase(); // prompt user to pick RPS
+        // set player and computer parameters
+        let computerSelection = computerPlay(); 
+        let result = playRound(playerSelection, computerSelection);
+        // set player and computer scores
+        // display round number
+        console.log(`Round ${i+1}`)
+        // checks winner
+        if (result == "computer"){
+            console.log(`Computer wins, ${computerSelection} beats ${playerSelection}`);
+            computerPoint++; // adds a point to computer
+        } else if (result == "player") {
+            console.log(`Player wins, ${playerSelection} beats ${computerSelection}`);
+            playerPoint++; // adds a point to player
+        } else if (result == 'tie'){
+            console.log(`It's a tie, both picked ${playerSelection}`);
+            i -= 1; // repeats round
+        } else { 
+            console.log("Try again")
+            i -= 1; // repeats round 
+        }
+        // output player and computer score
+        console.log(`Player-${playerPoint} Computer-${computerPoint}`);
+    } 
+    if (playerPoint > computerPoint) {
+        console.log("You Win! Congratulations")
+    } else {
+        console.log("You lose, Try Again?")
+    }
+}
+// call game loop
+console.log(game())
