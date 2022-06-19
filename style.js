b8d8722c22a170df6d405d116eac39e89ac69fc4
lfonk <jsharp4212@gmail.com>
2022-06-19 22:57:18 +0800

    Add game function for playing

--- style.js
+++ style.js
@@ -4,11 +4,58 @@ function computerPlay() {
     let pick = Math.floor(Math.random()*3)
     // output rock if zero
     if (pick == 0){
-        console.log("Rock");
+        return "Paper"
 
     } else if (pick == 1) { //output paper if 1
-        console.log("Paper");
+        return "Rock"
     } else {
-        console.log("Scissors"); // output scissors if 2
+        return "Scissors" // output scissors if 2
     }
+}
+
+// function that will play RPS (only 1 round)
+function game(){
+    let playerSelection = prompt("Rock, paper, or scissors? ","");
+    alert(`Player selected ${playerSelection}`);
+    let computerSelection = computerPlay();
+    alert(`Computer selected ${computerSelection}`);
+
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Paper"){
+            console.log("Computer Wins");
+        } else if (computerSelection == "Scissors"){
+            console.log("Player Wins");
+        } else { 
+            console.log("It's a tie, try again");
+        }
+    } else if (playerSelection == "Paper") {
+        if (computerSelection == "Paper"){
+            console.log("It's a tie, try again");
+        } else if (computerSelection == "Scissors"){
+            console.log("Computer Wins");
+        } else { 
+            console.log("Player Wins");
+        }
+    } else {
+        if (computerSelection == "Paper"){
+            console.log("Player Wins");
+        } else if (computerSelection == "Scissors"){
+            console.log("It's a tie, try again");
+        } else { 
+            console.log("Computer Wins");
+        }
+    }
+// if player is equal to paper comp scissors
+// output comp wins
+// if comp is rocl 
+// output player wins
+// if comp paper
+// output try again
+
+// if player picks scissors comp rock
+// output comp wins
+// comp picks paper
+// player wins
+// same input
+// output try again
 }
