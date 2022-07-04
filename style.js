c81def4d8469062cb016528a2a0ea10389b210eb
lfonk <jsharp4212@gmail.com>
2022-07-04 22:40:14 +0800

    Add UI

--- style.js
+++ style.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     game(button.id);
+    show(button.id);
   });
 }); //iterates each button
 
@@ -9,14 +10,6 @@ const scoreT = document.getElementById('scoreT')
 const roundT = document.getElementById('roundT')
 const text = document.getElementById('text')
 const winnerT = document.getElementById('winnerT')
-
-//const rock = document.createElement("img");
-//rock.addEventListener('click', ()=> {
-//    rock.setAttribute("src", )
-//})
-//const paper = document.createElement("img");
-//const scissors = document.createElement("img");
-
 
 let computerPoint = 0;
 let playerPoint = 0;
@@ -27,14 +20,43 @@ function computerPlay() {
     //Set RPS as numbers three numerical numbers 
     let pick = Math.floor(Math.random()*3)
     // output rock if zero
-    return (pick == 0) ?  "Paper" : (pick == 1) ? "Rock" : "Scissors"; 
+    if (pick == 0) {
+        document.getElementById('paperC').style.display = 'block';
+        document.getElementById('rockC').style.display = 'none';
+        document.getElementById('scissorsC').style.display = 'none';
+        return "Paper"
+    } else if (pick == 1) {
+        document.getElementById('rockC').style.display = 'block';
+        document.getElementById('paperC').style.display = 'none';
+        document.getElementById('scissorsC').style.display = 'none';
+        return "Rock"
+    } else {
+        document.getElementById('scissorsC').style.display = 'block';
+        document.getElementById('rockC').style.display = 'none';
+        document.getElementById('paperC').style.display = 'none';
+        return "Scissors"
+    }
+}
+
+function show(playerSelection) {
+    if (playerSelection == "rock") {
+        document.getElementById('rockP').style.display = 'block';
+        document.getElementById('paperP').style.display = 'none';
+        document.getElementById('scissorsP').style.display = 'none';
+    } else if (playerSelection == "paper") {
+        document.getElementById('paperP').style.display = 'block';
+        document.getElementById('rockP').style.display = 'none';
+        document.getElementById('scissorsP').style.display = 'none';
+    } else {
+        document.getElementById('scissorsP').style.display = 'block';
+        document.getElementById('rockP').style.display = 'none';
+        document.getElementById('paperP').style.display = 'none';
+    }
 }
 
 // function that will play RPS (only 1 round)
-function playRound(playerSelection, computerSelection){
-// set player and computer parameters 
-// compare player pick to computer pick 
-    if (playerSelection == "rock") { 
+function playRound(playerSelection, computerSelection){ 
+    if (playerSelection == "rock") {
         if (computerSelection == "Paper"){
             return "computer";
         } else if (computerSelection == "Scissors"){
@@ -50,7 +72,7 @@ function playRound(playerSelection, computerSelection){
         } else { 
             return "player";
         }
-    } else if (playerSelection == "scissors"){ 
+    } else { 
         if (computerSelection == "Paper"){
             return "player";
         } else if (computerSelection == "Scissors"){
@@ -58,51 +80,52 @@ function playRound(playerSelection, computerSelection){
         } else { 
             return "computer";
         }
-    } else { // player didnt pick from RPS
-        return "error";
-    }
+    } 
 }
 
 function winner() {
     if (computerPoint == 5 || playerPoint == 5) {
         
         if (playerPoint > computerPoint) {
-            computerPoint = 0;
-            playerPoint = 0; 
-            round = 1;
+            reset()
             return ("You Win! Congratulations")
             
         } else {
-            computerPoint = 0;
-            playerPoint = 0; 
-            round = 1;
+            reset()
             return ("You lose, Try Again?")
         }
-        
     } else {
         return ""
     }
 }
+
+function reset() {
+    computerPoint = 0;
+    playerPoint = 0; 
+    round = 1;
+}
+
+
 function game(playerSelection) {
     let computerSelection = computerPlay(); 
-        let result = playRound(playerSelection, computerSelection);
-        roundT.textContent = `Round ${round}`
-        if (result == "computer"){
-            text.textContent = `Computer wins, ${computerSelection} beats ${playerSelection}`;
-            computerPoint++;
-            round++; // adds a point to computer
-        } else if (result == "player") {
-            text.textContent = `Player wins, ${playerSelection} beats ${computerSelection}`;
-            playerPoint++; // adds a point to player
-            round++;
-        } else if (result == 'tie'){
-            text.textContent =`It's a tie, both picked ${playerSelection}`;
-            //i -= 1; // repeats round
-        } else { 
-            console.log("Try again")
-            //i -= 1; // repeats round 
-        }
-            // output player and computer score
-        scoreT.textContent = `Player-${playerPoint} Computer-${computerPoint}`;
-        winnerT.textContent = winner();
+    let result = playRound(playerSelection, computerSelection);
+    roundT.textContent = `Round ${round}`
+    if (result == "computer"){
+        text.textContent = `Computer wins, ${computerSelection} beats ${playerSelection}`;
+        computerPoint++;
+        round++; // adds a point to computer
+    } else if (result == "player") {
+        text.textContent = `Player wins, ${playerSelection} beats ${computerSelection}`;
+        playerPoint++; // adds a point to player
+        round++;
+    } else if (result == 'tie'){
+        text.textContent =`It's a tie, both picked ${playerSelection}`;
+        //i -= 1; // repeats round
+    } else { 
+        console.log("Try again")
+        //i -= 1; // repeats round 
+    }
+    // output player and computer score
+    scoreT.textContent = `Player-${playerPoint} Computer-${computerPoint}`;
+    winnerT.textContent = winner();
 }    
